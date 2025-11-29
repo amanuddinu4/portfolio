fd1f1ecc0087cf7db98481243d66bb1c10bd0167
amanuddinu4 <amanuddinu4@gmail.com>
2025-11-29 13:35:44 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  // 👇 Add these for GitHub Pages
+  output: 'export',        // ensures static HTML export
+  images: {
+    unoptimized: true      // required since GitHub Pages can't optimize images
+  },
+
 };
 
 module.exports = nextConfig;
-
-
